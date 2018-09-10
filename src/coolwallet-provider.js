88210c1b960b7d79f10570856a1f7d6502a8df24
Yi-Cyuan Chen <emn178@gmail.com>
2018-09-10 17:53:19 +0800

    update

--- src/coolwallet-provider.js
+++ src/coolwallet-provider.js
@@ -1,153 +1,239 @@
-(function () {
-  var WINDOW = typeof window === 'object';
-  var root = WINDOW ? window : {};
-  var WEB_WORKER = !WINDOW && typeof self === 'object';
-  var NODE_JS = typeof process === 'object' && process.versions && process.versions.node;
-  if (NODE_JS) {
-    root = global;
-  } else if (WEB_WORKER) {
-    root = self;
+const io = require('socket.io-client');
+const HookedWalletSubprovider = require('web3-provider-engine/subproviders/hooked-wallet');
+
+const WINDOW = typeof window === 'object';
+let root = WINDOW ? window : {};
+const NODE_JS = typeof process === 'object' && process.versions && process.versions.node;
+if (NODE_JS) {
+  root = global;
+}
+
+const PROTOCOL = 'coolwallet';
+
+function CoolwalletProvider(options) {
+  options = options || {};
+  this.apiKey = options.apiKey || '';
+  this.source = options.source || '';
+  this.callbackUrl = options.callbackUrl || '';
+  this.relayHost = options.relayHost || '';
+  this.requestId = new Date().getTime();
+  this.requests = {};
+  this.requestQueues = [];
+  this.processing = false;
+
+  this.getAccounts = this.getAccounts.bind(this);
+  this.signMessage = this.signMessage.bind(this);
+  this.signTransaction = this.signTransaction.bind(this);
+  this.session = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  this.socket = io(this.relayHost, { query: `session=${this.session}` });
+  this.socket.on('message', this.onMessage.bind(this));
+  if (WINDOW) {
+    this.div = document.createElement('div');
+    this.div.className = 'coolwallet-overlay';
+    this.div.style.display = 'none';
+    this.ok = document.createElement('a');
+    this.ok.innerHTML = 'Open CoolWallet';
+    this.ok.className = 'ok';
+    const cancel = document.createElement('a');
+    cancel.innerHTML = 'Cancel';
+    cancel.className = 'cancel';
+    cancel.onclick = this.cancel.bind(this);
+    cancel.href = 'javascript:;';
+    this.div.appendChild(cancel);
+    this.div.appendChild(this.ok);
+    this.iframe = document.createElement('iframe');
+    this.iframe.style.display = 'none';
+    document.body.appendChild(this.iframe);
+    document.body.appendChild(this.div);
   }
-  var COMMON_JS = typeof module === 'object' && module.exports;
+}
 
-  var PROTOCOL = 'coolwallet';
-
-  function CoolwalletProvider(options) {
-    options = options || {};
-    this.apiKey = options.apiKey || '';
-    this.source = options.source || '';
-    this.callbackUrl = options.callbackUrl || '';
-    this.relayHost = options.relayHost || '';
-    this.requestId = new Date().getTime();
-    this.requests = {};
-    this.requestQueues = [];
-    this.processing = false;
-
-    this.getAccounts = this.getAccounts.bind(this);
-    this.signMessage = this.signMessage.bind(this);
-    this.signTransaction = this.signTransaction.bind(this);
-
-    var io = this.getIo();
-    if (io) {
-      this.socket = io(this.relayHost);
-      this.socket.on('message', this.onMessage.bind(this));
-    } else {
-      console.error('can not find socket.io library.');
-    }
-  }
-
-  CoolwalletProvider.prototype.getIo = function () {
-    if (COMMON_JS) {
-      return require('socket.io-client');
-    } else {
-      return root.io;
-    }
-  };
-
-  CoolwalletProvider.prototype.onMessage = function (message) {
-    try {
-      message = JSON.parse(message);
-      var job = this.requests[message.requestId];
-      delete this.requests[message.requestId];
-      if (!job) {
-        return console.error('can not find request');
-      }
-      clearTimeout(job.timeout);
-      if (message.error) {
-        job.callback(new Error(message.error), message.result);
-      } else {
-        job.callback(null, message.result);
-      }
-    } catch (e) {
-      console.log(e, message);
-    }
-    this.processing = false;
-    this.dequeue();
-  };
-
-  CoolwalletProvider.prototype.getAccounts = function (callback) {
-    if (this.accounts) {
-      callback(null, [].concat(this.accounts));
-    } else {
-      var self = this;
-      this.request('getAccounts', function (error, result) {
-        if (!error) {
-          self.accounts = result;
-        }
-        callback(error, result)
-      });
-    }
-  };
-
-  CoolwalletProvider.prototype.signMessage = function (message, callback) {
-    this.request('signMessage', callback, message);
-  };
-
-  CoolwalletProvider.prototype.signTransaction = function (tx, callback) {
-    this.request('signTransaction', callback, tx);
-  };
-
-  CoolwalletProvider.prototype.request = function (action, callback, params) {
-    var requestId = this.requestId++;
-    params = params || {};
-    params.action = action;
-    var job = { requestId: requestId, callback: callback, params: params };
-    this.requestQueues.push(job);
-    this.dequeue();
-  };
-
-  CoolwalletProvider.prototype.dequeue = function () {
-    if (this.processing || this.requestQueues.length === 0) {
+CoolwalletProvider.prototype.onMessage = function (message) {
+  let job;
+  try {
+    message = JSON.parse(message);
+    if (!message) {
       return;
     }
-    this.processing = true;
-    var job = this.requestQueues.shift();
-    if (job.params.action === 'getAccounts' && this.accounts) {
-      this.processing = false;
-      return job.callback(null, [].concat(this.accounts));
+    job = this.requests[message.requestId];
+    delete this.requests[message.requestId];
+    this.socket.emit('recevied', message.requestId);
+    if (!job) {
+      return console.error('can not find request');
     }
-
-    this.requests[job.requestId] = job;
-    job.params.callback = this.createCallbackUrl(job.requestId);
-    job.uri = this.createUri(job.params);
-    var self = this;
-    job.timeout = setTimeout(function () {
-      delete self.requests[job.requestId];
-      callback(new Error('request timeout'), null);
-    }, 15000);
-    if (root.COOLWALLET_PROVIDER_URI_HANDLER) {
-      root.COOLWALLET_PROVIDER_URI_HANDLER(job);
-    } else if (WINDOW) {
-      window.open(job.uri);
+    clearTimeout(job.timeout);
+    if (message.error) {
+      const error = new Error(message.result);
+      error.code = message.error;
+      job.callback(error, null);
     } else {
-      console.error('not supported');
+      if (job.params.action === 'signMessage') {
+        message.result = `0x${message.result}`;
+      }
+      job.callback(null, message.result);
     }
-  };
-
-  CoolwalletProvider.prototype.createUri = function (params) {
-    Object.assign(params, {
-      apiKey: this.apiKey,
-      source: this.source
-    });
-    var query = Object.keys(params).map(function (key) {
-      return key + '=' + encodeURIComponent(params[key]);
-    }).join('&');
-    return PROTOCOL + '://?' + query;
-  };
-
-  CoolwalletProvider.prototype.createCallbackUrl = function (requestId) {
-    var joint = this.callbackUrl.indexOf('?') === -1 ? '?' : '&';
-    return this.callbackUrl + joint + 'session=' + this.socket.id + '&requestId=' + requestId;
-  };
-
-  if (COMMON_JS) {
-    module.exports = CoolwalletProvider;
-  } else {
-    root.CoolwalletProvider = CoolwalletProvider;
-    if (AMD) {
-      define(function () {
-        return CoolwalletProvider;
-      });
+  } catch (e) {
+    console.log(e, message);
+    if (job) {
+      job.callback(e, null);
     }
   }
-})();
+  this.processing = false;
+  if (WINDOW) {
+    this.div.style.display = 'none';
+  }
+  this.dequeue();
+};
+
+CoolwalletProvider.prototype.checkReady = function () {
+  if (this.socket.connected) {
+    return true;
+  }
+  this.socket.once('connect', function () {
+    this.dequeue();
+  }.bind(this));
+};
+
+CoolwalletProvider.prototype.getAccounts = function (callback) {
+  if (this.accounts) {
+    callback(null, [].concat(this.accounts));
+  } else {
+    const self = this;
+    this.request('getAccounts', function (error, result) {
+      if (!error) {
+        self.accounts = result;
+      }
+      callback(error, result);
+    });
+  }
+};
+
+CoolwalletProvider.prototype.signMessage = function (message, callback) {
+  if (typeof message.data === 'object') {
+    message.raw = message.data.raw;
+    message.data = message.data.data;
+  }
+  this.request('signMessage', callback, message);
+};
+
+CoolwalletProvider.prototype.signTransaction = function (tx, callback) {
+  this.request('signTransaction', callback, tx);
+};
+
+CoolwalletProvider.prototype.request = function (action, callback, params) {
+  const requestId = this.requestId++;
+  params = params || {};
+  params.action = action;
+  const job = { requestId: requestId, callback: callback, params: params };
+  this.requestQueues.push(job);
+  this.dequeue();
+};
+
+CoolwalletProvider.prototype.dequeue = function () {
+  if (this.processing || this.requestQueues.length === 0) {
+    return;
+  }
+  if (!this.checkReady()) {
+    return;
+  }
+  this.processing = true;
+  const job = this.requestQueues.shift();
+  if (job.params.action === 'getAccounts' && this.accounts) {
+    this.processing = false;
+    return job.callback(null, [].concat(this.accounts));
+  }
+
+  this.requests[job.requestId] = job;
+  job.params.callback = this.createCallbackUrl(job.requestId);
+  job.uri = this.createUri(job.params);
+  job.timeout = setTimeout(() => {
+    delete this.requests[job.requestId];
+    this.processing = false;
+    this.div.style.display = 'none';
+    job.callback(new Error('request timeout'), null);
+  }, 600000);
+  this.currentJob = job;
+  if (root.COOLWALLET_PROVIDER_URI_HANDLER) {
+    root.COOLWALLET_PROVIDER_URI_HANDLER(job);
+  } else if (WINDOW) {
+    // if (job.params.action === 'signTransaction') {
+      this.ok.href = job.uri;
+      this.div.style.display = 'block';
+    // } else {
+    //   this.iframe.src = job.uri;
+    // }
+  } else {
+    console.error('not supported');
+  }
+};
+
+CoolwalletProvider.prototype.createUri = function (params) {
+  Object.assign(params, {
+    apiKey: this.apiKey,
+    source: this.source,
+    t: Date.now()
+  });
+  const query = Object.keys(params).map(function (key) {
+    return key + '=' + encodeURIComponent(params[key]);
+  }).join('&');
+  return PROTOCOL + '://?' + query;
+};
+
+CoolwalletProvider.prototype.createCallbackUrl = function (requestId) {
+  const joint = this.callbackUrl.indexOf('?') === -1 ? '?' : '&';
+  return this.callbackUrl + joint + 'session=' + this.session + '&requestId=' + requestId;
+};
+
+CoolwalletProvider.prototype.stop = function () {
+  console.log('stop');
+  if (this.socket) {
+    this.socket.close();
+    this.socket = null;
+  }
+  if (WINDOW) {
+    document.body.removeChild(this.iframe);
+    this.iframe = null;
+  }
+};
+
+CoolwalletProvider.prototype.cancel = function () {
+  if (!this.currentJob) {
+    return;
+  }
+  const job = this.currentJob;
+  delete this.requests[job.requestId];
+  this.processing = false;
+  job.callback(new Error('cancelled'), null);
+  this.div.style.display = 'none';
+};
+
+ function createCoolwalletProvider(options) {
+  const collwalletProvider = new CoolwalletProvider(options);
+  let timer;
+  const startTimer = function (provider) {
+    if (timer) {
+      return;
+    }
+    timer = setInterval(() => {
+      if (!provider.engine._blockTracker._isRunning) {
+        clearInterval(timer);
+        timer = null;
+        collwalletProvider.stop();
+      }
+    }, 5000);
+  };
+  return new HookedWalletSubprovider({
+    getAccounts: function (callback) {
+      startTimer(this);
+      collwalletProvider.getAccounts(callback);
+    },
+    signMessage: function(message, callback) {
+      collwalletProvider.signMessage(message, callback);
+    },
+    signTransaction: function (tx, callback) {
+      collwalletProvider.signTransaction(tx, callback);
+    }
+  });
+}
+
+module.exports = createCoolwalletProvider;
--- test/index.js
+++ test/index.js
@@ -2,19 +2,19 @@ const qs = require('qs');
 const request = require('request');
 const Web3 = require('web3');
 const ProviderEngine = require('web3-provider-engine');
-const HookedWalletSubprovider = require('web3-provider-engine/subproviders/hooked-wallet');
 const RpcSubprovider = require('web3-provider-engine/subproviders/rpc');
-const CoolwalletProvider = require('../src/coolwallet-provider');
+const createCoolwalletProvider = require('../src/coolwallet-provider');
 
 const engine = new ProviderEngine();
 const web3 = new Web3(engine);
 
-engine.addProvider(new HookedWalletSubprovider(new CoolwalletProvider({
+engine.addProvider(createCoolwalletProvider({
   apiKey: 'apiKey',
   source: 'JOYSO',
   callbackUrl: 'http://localhost:8888/',
   relayHost: 'ws://localhost:8888/'
-})));
+}));
+
 engine.addProvider(new RpcSubprovider({ rpcUrl: 'https://ropsten.infura.io/EzVXM6CfAPpr5ytQ9v9c' }));
 
 engine.start();
@@ -40,9 +40,13 @@ global.COOLWALLET_PROVIDER_URI_HANDLER = (job) => {
       break;
     case 'signTransaction':
       message = {
-        error: 'gas price is too low',
-        result: null
+        error: '12345',
+        result: 'gas price is too low'
       };
+      // message = {
+      //   error: null,
+      //   result: '0xadc6b8bd10a9fa31594722bea9961530027a4929bbd97edaf966525a8297f584'
+      // };
       break;
   }
   request.post(params.callback, { json: message }, (error, response, body) => {
@@ -56,7 +60,7 @@ web3.eth.getAccounts((error, result) => {
   console.log(error, result);
 });
 
-web3.eth.sign('0x212b1B5aFa70e74E461a7eBF7f17d85A465d3394', '0xdc8c5bce79bcb47e9b202aaab64f1160d8de6f16afb9c63bcbab481b493b1c62dc8c5bce79bcb47e9b202aaab64f1160d8de6f16afb9c63bcbab481b493b1c62dc8c5bce79bcb47e9b202aaab64f1160d8de6f16afb9c63bcbab481b493b1c62dc8c5bce79bcb47e9b202aaab64f1160d8de6f16afb9c63bcbab481b493b1c62dc8c5bce79bcb47e9b202aaab64f1160d8de6f16afb9c63bcbab481b493b1c62dc8c5bce79bcb47e9b202aaab64f1160d8de6f16afb9c63bcbab481b493b1c62dc8c5bce79bcb47e9b202aaab64f1160d8de6f16afb9c63bcbab481b493b1c62', (error, result) => {
+web3.eth.sign('0x212b1B5aFa70e74E461a7eBF7f17d85A465d3394', { data: '0x1234', raw: 'plain' }, (error, result) => {
   console.log(error, result);
 });
 
